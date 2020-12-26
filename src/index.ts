254fa3f0717373edd107849fe72417104368f770
delta-kor <deltakbot1@gmail.com>
2020-12-26 19:25:51 +0900

    Add missing exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import Link from './components/link';
 import TextItem from './components/text-item';
 import TitleDesc from './components/title-desc';
 import FeedContent from './content/feed';
+import ButtonType from './button-type';
+import DisplayType from './display-type';
 import MessageInfo from './message-info';
 import MessageTemplate from './message-template';
 import MessageType from './message-type';
@@ -13,6 +15,8 @@ export {
   TextItem,
   TitleDesc,
   FeedContent,
+  ButtonType,
+  DisplayType,
   MessageInfo,
   MessageTemplate,
   MessageType,
